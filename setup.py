3215cb55d75af31093db03882a8cba83dff7b2ce
Shailesh Appukuttan <appukuttan.shailesh@gmail.com>
2017-06-07 21:29:58 +0200

    Updating setup.py

--- setup.py
+++ setup.py
@@ -16,15 +16,7 @@ setup(
     license='MIT',
     description='A SciUnit library for data-driven testing of neuronal morphologies.',
     long_description="",
-    install_requires=['scipy>=0.17',
-                      'matplotlib>=1.5',
-                      'neo==0.4',
-                      'elephant',
-                      'sciunit==0.1.5.8',
-                      'allensdk==0.12.4.1',
-                      'pyneuroml>=0.2.3',
-                      'scoop'],
-    dependency_links = ['https://github.com/scidash/sciunit/tarball/dev#egg=sciunit-0.1.5.8',
-                        'https://github.com/rgerkin/AllenSDK/tarball/python3.5#egg=allensdk-0.12.4.1',
-                        'https://github.com/rgerkin/pyNeuroML/tarball/master#egg=pyneuroml-0.2.3']
+    install_requires=['neo','elephant','sciunit>=0.1.5.2',],
+    dependency_links = ['git+http://github.com/neuralensemble/python-neo.git#egg=neo-0.4.0dev',
+                        'https://github.com/scidash/sciunit/tarball/dev']
 )
