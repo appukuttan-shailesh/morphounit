ed5a4deef292ede7e7157f272483e4a9e2d04b2f
Pedro Garcia <pedro.garcia@unic.cnrs-gif.fr>
2018-03-23 16:27:57 +0100

    Updating capability files for a morphological feature test

--- morphounit/capabilities/cap_ProvidesMorphFeatureInfo.py
+++ morphounit/capabilities/cap_ProvidesMorphFeatureInfo.py
@@ -11,13 +11,13 @@ class ProvidesMorphFeatureInfo(sciunit.Capability):
     def get_morph_feature_info(self):
         """
         Must return a dictionary of the form:
-            { 'cell_name': { 'morph_feature_name_1': {'value': 'XX um'} },
-                           { 'morph_feature_name_2': {'value': 'XX um'} }
-                           ...
-            }
+        {'cell_name': { 'morph_feature_name_1': {'value': ['X0_1 um', 'X0_2 um', ...] }},
+                      { 'morph_feature_name_2': {'value': ['X1_1 um', 'X1_2 um', ...] }}
+                      ...
+        }
         """
         raise NotImplementedError()
 
     def get_morph_feature_value(self):
-        """ returns the morphological feature in a model of a reconstructed neuron """
+        """ returns the morphological feature in a model of a digitally reconstructed neuron """
         return self.get_morph_feature_info()
