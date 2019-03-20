659d50adecea6e7dd8f3447c3605e21fda4349da
Pedro Garcia <pedro.garcia@unic.cnrs-gif.fr>
2019-03-20 15:26:42 +0100

    Minor fixes to reorganize the code betwenn the Model and the Test classes

--- morphounit/tests/test_NeuroM_MorphStats.py
+++ morphounit/tests/test_NeuroM_MorphStats.py
@@ -27,10 +27,10 @@ class NeuroM_MorphStats_Test(sciunit.Test):
         observation = self.format_data(observation)
         sciunit.Test.__init__(self, observation, name)
 
-
     # ----------------------------------------------------------------------
 
     def format_data(self, data):
+
         """
         This accepts data input in the form:
         ***** (observation) *****
@@ -104,62 +104,10 @@ class NeuroM_MorphStats_Test(sciunit.Test):
         self.morp_path = model.morph_path
         mod_prediction = model.get_morph_feature_info()
 
-        mapping = lambda section: section.points
-        for cell_ID, dict0 in mod_prediction.items():  # Dict. with cell's morph_path-features dict. pairs for each cell
-
-            # Adding more neurite's features:
-            # field diameter, bounding-box -X,Y,Z- extents and -largest,shortest- principal extents
-            if os.path.isdir(self.morp_path):
-                neuron_path = os.path.join(self.morp_path, cell_ID)
-            else:
-                neuron_path = self.morp_path
-            neuron_model = nm.load_neuron(neuron_path)
-            for key1, dict1 in dict0.items():  # Dict. with feature name-value pairs for each cell part:
-                                                # soma, apical_dendrite, basal_dendrite or axon
-                if any(sub_str in key1 for sub_str in ['axon', 'dendrite']):
-                    cell_part = key1
-                    filter = lambda neurite: neurite.type == getattr(nm.NeuriteType, cell_part)
-                    neurite_points = [p for p in nm.iter_neurites(neuron_model, mapping, filter)]
-                    neurite_points = np.concatenate(neurite_points)
-                    neurite_cloud = neurite_points[:, 0:3]
-
-                    # Compute the neurite's bounding-box -X,Y,Z- extents
-                    neurite_X_extent, neurite_Y_extent, neurite_Z_extent = \
-                        np.max(neurite_cloud, axis=0) - np.min(neurite_cloud, axis=0)
-                    dict1.update({"neurite_X_extent": neurite_X_extent})
-                    dict1.update({"neurite_Y_extent": neurite_Y_extent})
-                    dict1.update({"neurite_Z_extent": neurite_Z_extent})
-
-                    # Compute the neurite's -largest, shortest- principal extents
-                    principal_extents = sorted(nm.morphmath.principal_direction_extent(neurite_cloud))
-                    dict1.update({"neurite_shortest_extent": principal_extents[0]})
-                    dict1.update({"neurite_largest_extent": principal_extents[-1]})
-
-                    # Compute the neurite-field diameter
-                    # neurite_field_diameter = nm.morphmath.polygon_diameter(neurite_cloud)
-                    # dict1.update({"neurite_field_diameter": neurite_field_diameter})
-
         """
         with open(model.output_file, 'w') as fp:
             print json.dump(mod_prediction, fp, sort_keys=True, indent=3)
         """
-
-        dim_um = ['radius', 'radii', 'diameter', 'length', 'distance', 'extent']
-        for dict1 in mod_prediction.values():  # Set of cell's part-features dictionary pairs for each cell
-
-            # Adding the right units and converting feature values to strings
-            for dict2 in dict1.values():  # Dict. with feature name-value pairs for each cell part:
-                                            # soma, apical_dendrite, basal_dendrite or axon
-                for key, val in dict2.items():
-                    if any(sub_str in key for sub_str in ['radius', 'radii']):
-                        del dict2[key]
-                        val *= 2
-                        key = key.replace("radius", "diameter")
-                        key = key.replace("radii", "diameter")
-                    if any(sub_str in key for sub_str in dim_um):
-                        dict2[key] = dict(value=str(val) + ' um')
-                    else:
-                        dict2[key] = dict(value=str(val))
 
         self.prediction_txt = copy.deepcopy(mod_prediction)
 
